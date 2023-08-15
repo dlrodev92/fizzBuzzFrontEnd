1428d07a6ca0fe4088a0b621f0a6f3fe580d7a05
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-08-15 14:23:20 +0100

    add components layout, add responsive

--- src/components/LoginContainer.jsx
+++ src/components/LoginContainer.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react"
+
 export default function LoginContainer(){
+    const [user, setUser] = useState({})
+    const handleChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
     return(
-        <div className="bg-blue-300 h-[80%] w-[48%] rounded-xl shadow-xl flex flex-col items-center justify-center gap-5">
+        <div className="bg-blue-300 h-[80%] w-[90%] md:w-[48%] rounded-xl shadow-xl flex flex-col items-center justify-center gap-5">
             <h1 className="text-4xl font-bold">Login</h1>
+                <form
+                 className="w-[80%]  md:w-[50%] h-[70%] md:h-[50%] bg-slate-50 rounded-lg flex flex-col gap-3 items-center justify-start"
+                 
+                 >
+                    <label className="text-lg font-thin mt-10">Username</label>
+                    <input
+                     className="bg-slate-50 w-[60%] border-2 border-blue-300 rounded shadow-xl outline-none"
+                     type="text"
+                     placeholder="Username"
+                     name="username"
+                     onChange={handleChange} 
+                     required
+                      />
+                    <label className="text-lg font-thin">Password</label>
+                    <input
+                     className="bg-slate-50 w-[60%] border-2 border-blue-300 rounded shadow-xl outline-none"
+                     type="password"
+                     placeholder="Password"
+                     name="password"
+                     onChange={handleChange} 
+                     required
+                      />
+                    <button className="text-lg font-bold w-[150px] h-[40px] bg-blue-300 rounded-lg mt-10 hover:bg-slate-50 hover:scale-105 hover:border-2 hover:border-blue-300 ">Login</button>
+                </form>
         </div>
     )
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,9 +2,9 @@ import FizzBuzzContainer from "../components/FizzBuzzContainer"
 import LoginContainer from "../components/LoginContainer"
 export default function Main(){
     return(
-    <div className="w-screen h-screen flex items-center justify-center gap-5">
-        <FizzBuzzContainer/>
+    <div className="w-screen h-screen flex flex-col md:flex-row items-center justify-center gap-5 p-3">
         <LoginContainer/>
+        <FizzBuzzContainer/>
     </div>
     )
 }
